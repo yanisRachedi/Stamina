0706902c574c257b1ed07f12ba58996dea355136
yanisrachedi <yanis.rachedi@renault.com>
2024-02-20 12:15:00 +0100

    Stamina project - test initial commit

---
+++ stamina/src/main/java/com/example/stamina/StaminaApplication.java
@@ -0,0 +1,13 @@
+package com.example.stamina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaminaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaminaApplication.class, args);
+	}
+
+}
